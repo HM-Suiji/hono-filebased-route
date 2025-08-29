f80a801c033f605c3064530cc3f78dd4b7e4cb5a
Suiji <1704802092@qq.com>
2025-08-29 15:02:45 +0800

    Revert "docs: fix 404"
    This reverts commit 9173031a0947df6073fdd82c3766cc5379c13630.

--- .vercel/deploy.sh
+++ .vercel/deploy.sh
@@ -1,5 +1,1 @@
-[ "$VERCEL_GIT_COMMIT_REF" != "main" ] \
-&& echo "Not on main branch. Skipping build." && exit 0 \
-|| git diff --quiet HEAD^ HEAD .vercel/version.txt \
-&& echo ".vercel/version.txt unchanged. Skipping build." && exit 0 \
-|| echo ".vercel/version.txt changed on main branch. Building..." && exit 1
+git diff --quiet HEAD^ HEAD .vercel/version.txt && echo ".vercel/version.txt unchanged. Skipping build." && exit 0 || echo ".vercel/version.txt changed. Building..." && exit 1
